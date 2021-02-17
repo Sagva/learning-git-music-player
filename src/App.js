5bf305bfcdaae06c1f558217e90e40cbebc48d00
Elena <sagva2014@gmail.com>
2021-02-17 13:34:19 +0100

    Added BrowserRouter, Route and NavLink to navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import Home from "./components/Home";
+import {BrowserRouter, Route} from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Navbar/>
+        <Route exact path='/' component={Home} />
+      </BrowserRouter>
     </div>
   );
 }
